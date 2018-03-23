8f3d1de8988961b1ba49b49d2d4e22286806b228
dylanshuler11 <slyCooper1!>
2018-03-22 17:13:33 -0700

    added junit stuff

---
+++ app/src/test/java/com/example/manalili18/quoridorgamestate/GameStateTest.java
@@ -0,0 +1,62 @@
+package com.example.manalili18.quoridorgamestate;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by shuler18 on 3/22/2018.
+ */
+public class GameStateTest {
+    @Test
+    public void TestToString() throws Exception {
+
+    }
+
+    @Test
+    public void movePawn() throws Exception {
+//d
+        
+    }
+
+    @Test
+    public void moveUp() throws Exception {
+//p
+    }
+
+    @Test
+    public void moveDown() throws Exception {
+//d
+    }
+
+    @Test
+    public void moveLeft() throws Exception {
+//p
+    }
+
+    @Test
+    public void moveRight() throws Exception {
+//d
+    }
+
+    @Test
+    public void finalizeTurn() throws Exception {
+
+    }
+
+    @Test
+    public void undo() throws Exception {
+
+    }
+
+    @Test
+    public void placeWall() throws Exception {
+
+    }
+
+    @Test
+    public void rotateWall() throws Exception {
+
+    }
+
+}
